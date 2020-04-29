2a15029098b435e6b99061bcd5d07eb5287ae831
Allencl <Allen__cc@163.com>
2020-04-29 11:10:11 +0800

    1

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// x-response-time
 
+app.use(async (ctx, next) => {
+    console.log(111);
+    const start = Date.now();
+    await next();
+    console.log(222);
+
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${321}ms`);
+});
+
+app.use(async (ctx, next) => {
+    console.log(333);
+
+    await next();
+    console.log(444);
+});
 
 app.use( async ctx=>{
     // console.log('nini');
+    console.log(555);
+
     ctx.body = 'Hello World';
 });
 
